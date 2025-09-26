f03cb6344c701c53ff87aa23b7c31e7d6354364f
mukesh1234ym-png <mukesh1234.ym@gmail.com>
2025-09-26 13:21:59 +0530

    Create Square cube.cpp

---
+++ Square cube.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath> // For pow()
+int main ( {
+int n;
+std::cout << "Enter the value of n: "; std::cin >> n;
+std::cout << "InResults from 1 to" << n <<
+":In";
+for (int i = 1; 1 <= n; ++i) 1
+if (% 2 == 0) 1
+// Even number: print cube
+std::cout << "Even:" << i <<"^3="<<
+pow(i, 3) << std::end;
+yelse 1
+#/ Odd number: print square
+std::cout < "Odd:" <<¡«<"^2="<<
+pow(i, 2) << std::endl;
+}
+ 
+}
+return 0;
+}
